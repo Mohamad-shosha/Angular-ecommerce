d52b030099b6a699d24c938c85e4a507b43aa883
FaresAlatrosh <142522651+FaresAlatrosh@users.noreply.github.com>
2025-03-12 02:19:58 +0200

    feat: Add product-category and category-based filtering
    - Added `product-category` in the common module for shared usage.
    - Created `ProductCategoryMenu` component with a sidebar menu.
    - Sidebar menu fetches category data (name & ID) from the API via `ProductService`.
    - Updated routing in `app.module` to support searching by category name.
    - Modified `app.component.html` to integrate the `product-category` form.
    - Implemented dynamic category-based filtering in the application.

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -6,15 +6,17 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-
   products: Product[] = [];
   currentCategoryId: number = 1;
+  currentCategoryName: string = '';
 
-  constructor(private productService: ProductService,
-              private route: ActivatedRoute) { }
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe(() => {
@@ -23,25 +25,24 @@ export class ProductListComponent implements OnInit {
   }
 
   listProducts() {
-
     // check if "id" parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
     if (hasCategoryId) {
       // get the "id" param string. convert string to a number using the "+" symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
-    }
-    else {
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name')!;
+    } else {
       // not category id available ... default to category id 1
       this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
     }
 
     // now get the products for the given category id
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      data => {
+    this.productService
+      .getProductList(this.currentCategoryId)
+      .subscribe((data) => {
         this.products = data;
-      }
-    )
+      });
   }
-
 }

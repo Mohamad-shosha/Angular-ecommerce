3ec775ce3440b37e6b0c92a06ebb6ea341762424
FaresAlatrosh <142522651+FaresAlatrosh@users.noreply.github.com>
2025-03-15 02:22:45 +0200

    feat: Implement search functionality by keyword
    - Created a new `SearchComponent` for searching products by keyword.
    - Updated `app.module.ts` to add routing for `search/:keyword`.
    - Designed the `search.component.html` with an input field and search button.
    - Integrated `<app-search>` in `app.component.html` for global search access.
    - Added search route handling in TypeScript.
    - Updated `product-list.component.ts`:
      - Implemented `handleSearchProducts()` to fetch products by keyword.
      - Implemented `handleListProducts()` to fetch products by category ID.
    - Extended `ProductService` with `findByNameContaining()` to query the API for search results.

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -14,13 +14,24 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) {}
 
+  private getProducts(searchUrl: string): Observable<Product[]> {
+    return this.httpClient
+      .get<GetResponseProducts>(searchUrl)
+      .pipe(map((response) => response._embedded.products));
+  }
+
   getProductList(theCategoryId: number): Observable<Product[]> {
     // need to build URL based on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-    return this.httpClient
-      .get<GetResponseProducts>(searchUrl)
-      .pipe(map((response) => response._embedded.products));
+    return this.getProducts(searchUrl);
+  }
+
+  searchProduct(theKeyword: string): Observable<Product[]> {
+    // need to build URL based on the keyword
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+
+    return this.getProducts(searchUrl);
   }
 
   getProductCategories(): Observable<ProductCategory[]> {

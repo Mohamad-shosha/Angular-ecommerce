91baeb53b65f88d526187e9e3f58991cde9264be
Mohamed Shosha <moshosha267@gmail.com>
2025-04-25 01:03:35 +0300

    feat: add product detail component and fetch product by ID functionality
    - Added folder trousers that contain images of trousers in assests
    - Created ProductDetailComponent to display detailed product information.
    - Added getProduct(id) method in ProductService to fetch a product by its ID.
    - Updated Product model in common to include product ID for data mapping.

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -53,7 +53,7 @@ export class ProductListComponent implements OnInit {
     } else {
       // not category id available ... default to category id 1
       this.currentCategoryId = 1;
-      this.currentCategoryName = 'Books';
+      this.currentCategoryName = 'T-Shirts';
     }
 
     // now get the products for the given category id
